7808b83136503e7f076dfbdbb5578d5720dc9720
lirui.12 <lirui.12@bytedance.com>
2020-08-29 12:39:22 +0800

    注册接口
    Change-Id: I409c667a75a275f017b4b92bd3804639680fa245

--- src/main/java/com/software/web/controller/LikeDataController.java
+++ src/main/java/com/software/web/controller/LikeDataController.java
@@ -10,6 +10,7 @@ import com.software.util.StringUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +20,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
+@Controller
 public class LikeDataController {
     @Autowired
     LikeDataService likeDataService;
